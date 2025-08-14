33e345d47fa6f177277322996aa24b2473e572a2
Jorge Martinez-Lopez <martinezlopezjorge35@gmail.com>
2025-08-13 19:43:51 -0500

    deleted comment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
       serviceID: 'service_xj6swc5',
       templateID: 'template_bts4s8f',      // booking template (unchanged)
       contactTemplateID: 'template_f6adlbi',     // contact template (new)
-      publicKey: '2vHPznwKNhlOV9nlr' // placeholder will add actual publicKey later
+      publicKey: '2vHPznwKNhlOV9nlr'
     }
   };
   
